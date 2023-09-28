5e342e3baf340d48057449c5978b802ebf262793
gur22-09 <gurprit.iiest@gmail.com>
2023-09-28 18:54:46 +0530

    feat: make apis secure

--- src/slack.ts
+++ src/slack.ts
@@ -1,14 +1,76 @@
-import type { Handler } from '@netlify/functions';
+import type { Handler } from "@netlify/functions";
 
-export const handler: Handler = async () => {
-	// TODO validate the Slack request
+import { parse } from "querystring";
+import {
+  getRandomCatImageUrl,
+  slackApi,
+  verifySlackRequest,
+} from "./util/slack";
 
-	// TODO handle slash commands
+async function handleSlashCommand(payload: SlackSlashCommandPayload) {
+  switch (payload.command) {
+    case "/foodfight":
+      const catUrl = await getRandomCatImageUrl();
+      const response = await slackApi("chat.postMessage", {
+        channel: payload.channel_id,
+        attachments: [
+          {
+            fallback: "Plain-text summary of the attachment.",
+            color: "#2eb886",
+            pretext: "Don't you just love cats",
+            fields: [
+              {
+                title: "Priority",
+                value: "High",
+                short: false,
+              },
+            ],
+            image_url: catUrl,
+            thumb_url: "https://cdn2.thecatapi.com/images/YXjvj-hjW.jpg",
+          },
+        ],
+        text: "Things are happening!",
+      });
 
-	// TODO handle interactivity (e.g. context commands, modals)
+      if (!response.ok) {
+        console.log(response);
+      }
+      break;
 
-	return {
-		statusCode: 200,
-		body: 'TODO: handle Slack commands and interactivity',
-	};
+    default:
+      return {
+        statusCode: 200,
+        body: `Command ${payload.command} is not recognized`,
+      };
+  }
+
+  return {
+    statusCode: 200,
+    body: "",
+  };
+}
+
+export const handler: Handler = async (event) => {
+  // validate the Slack request
+  const isValid = verifySlackRequest(event);
+  if (!isValid) {
+    console.error("invalid request");
+    return {
+      statusCode: 400,
+      body: "invalid request",
+    };
+  }
+
+  // TODO handle slash commands
+  const body = parse(event.body ?? "") as SlackPayload;
+
+  if (body) {
+    return handleSlashCommand(body as SlackSlashCommandPayload);
+  }
+  // TODO handle interactivity (e.g. context commands, modals)
+
+  return {
+    statusCode: 200,
+    body: "TODO: Lets build an Awesome Slack bot!",
+  };
 };
--- src/util/slack.ts
+++ src/util/slack.ts
@@ -1,1 +1,41 @@
-// TODO create Slack utilities
+import type { HandlerEvent } from "@netlify/functions";
+import { createHmac } from "crypto";
+
+export function slackApi(
+  endpoint: SlackApiEndpoint,
+  body: SlackApiRequestBody
+) {
+  return fetch(`https://slack.com/api/${endpoint}`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_BOT_OAUTH_TOKEN}`,
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(body),
+  }).then((res) => res.json());
+}
+
+export function verifySlackRequest(request: HandlerEvent) {
+  const secret = process.env.SLACK_SIGNING_SECRET!;
+  const signature = request.headers["x-slack-signature"];
+  const timestamp = Number(request.headers["x-slack-request-timestamp"]);
+  const now = Math.floor(Date.now() / 1000); // precison of slack timestamp is smaller than js timestamp
+
+  if (Math.abs(now - timestamp) > 300) {
+    return false;
+  }
+
+  const hash = createHmac("sha256", secret)
+    .update(`v0:${timestamp}:${request.body}`)
+    .digest("hex");
+
+  return `v0=${hash}` === signature;
+}
+
+export async function getRandomCatImageUrl() {
+  return fetch(`https://api.thecatapi.com/v1/images/search`, {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((data) => data[0].url);
+}
